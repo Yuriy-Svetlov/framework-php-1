0e29794896d25f957ff956d137bc91bba78ac205
semiromid <startpascal@mail.ru>
2020-05-15 13:41:09 +0300

    0.1.3

--- views/index/index.php
+++ views/index/index.php
@@ -11,12 +11,12 @@ $h2 = Sanitize::html_sanitize($data["data"]["view"]["h2"]);
 		<?php echo $h2; ?>
 	</h2>
 
-	<a target="_blank" href="//my-framework-php/AdFGFggGHhhHHyhhhbfi9878IK/debug_panhel">Debug panel</a>
+	<a target="_blank" href="/AdFGFggGHhhHHyhhhbfi9878IK/debug_panhel">Debug panel</a>
 	<br>
 	<br>
 	Examples:
 	<br>
-	<a target="_blank" href="//my-framework-php/my">my-framework-php/my</a>
+	<a target="_blank" href="/my">my-framework-php/my</a>
 	<br>
-	<a target="_blank" href="//my-framework-php/my/post">my-framework-php/my/post</a>
+	<a target="_blank" href="/my/post">my-framework-php/my/post</a>
 </div>
