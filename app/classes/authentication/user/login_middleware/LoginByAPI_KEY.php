456edb452533f187401d8c7324665020c6ddf40c
semiromid <startpascal@mail.ru>
2020-06-24 20:17:48 +0300

    0.2.7

--- app/classes/authentication/user/login_middleware/LoginByAPI_KEY.php
+++ app/classes/authentication/user/login_middleware/LoginByAPI_KEY.php
@@ -42,7 +42,25 @@ class LoginByAPI_KEY extends \approot\classes\authentication\user\LoginMiddlewar
     */
     private static function verifyRequestHeaders(): bool{
 
+
+        if (!function_exists('getallheaders')){
+
+            function getallheaders(){
+                $headers = [];
+                foreach ($_SERVER as $name => $value)
+                {
+
+                   if (substr($name, 0, 5) == 'HTTP_'){
+                       $headers[str_replace(' ', '-', ucwords(strtolower(str_replace('_', ' ', substr($name, 5)))))] = $value;
+                   }
+               }
+               
+               return $headers;
+            }
+        }
+
         $headers = getallheaders();
+
         if(is_array($headers) === false){
             \approot\classes\Logger::sendToLog("getallheaders() is not array [login] Class ".__CLASS__);
 
